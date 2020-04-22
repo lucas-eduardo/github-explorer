e142ffa590df7575af584ca743494c7dfa820432
Lucas Eduardo Santos da Silva <lucas.eduardo.developer@gmail.com>
2020-04-22 09:02:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h1 align="center">
+  Projeto Github Explorer
+</h1>
+
+## Sobre o projeto
+Projeto inicial para obter os primeiros conhecimentos em ReactJS.
+
+<img src=".github/image1.png" width="600">
+<hr>
+<img src=".github/image2.png" width="600">
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const sessionRepositories = sessionStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (sessionRepositories) {
+      return JSON.parse(sessionRepositories);
+    }
+
+    return [];
+  });
+  const [inputError, setInputError] = useState('');
+  const [newRepo, setNewRepo] = useState('');
+
+  useEffect(() => {
+    sessionStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const { data } = await api.get<Repository>(`repos/${newRepo}`);
+
+      setRepositories([...repositories, data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Expore repositórios no Github</Title>
+
+      <Form onSubmit={handleAddRepository} hasError={!!inputError}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
